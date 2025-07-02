c59728eaff818df5e263e4e0789b8d20dcfb2eeb
gaevuha <gaevuha@gmail.com>
2025-07-02 08:42:12 +0300

    add module-03 fix4

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -8,7 +8,7 @@ interface FetchMoviesResp {
   total_pages: number;
 }
 
-const ACCESS_TOKEN = import.meta.env.VITE_TMDB_TOKEN;
+const ACCESS_TOKEN = import.meta.env.VITE_TMDB_API_KEY;
 
 export const fetchMovies = async (
   query: string,
